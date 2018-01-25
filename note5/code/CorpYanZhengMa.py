b56718b4e1afd6cfe189bfcd39dce953c0d24f0e
Peibin Wang <yeyupiaoling@foxmail.com>
2018-01-25 21:12:21 +0800

    添加遗漏的文件

---
+++ note5/code/CorpYanZhengMa.py
@@ -0,0 +1,57 @@
+# coding=utf-8
+import os
+import uuid
+
+from PIL import Image
+
+
+class YanZhenMaUtil():
+    def __init__(self):
+        pass
+
+    def splitimage(self,src, dstpath):
+        # 分割路径,并获得文件名
+        name = src.split('/')
+        name1 = name[name.__len__() - 1]
+        name2 = name1.split('.')[0]
+        # 加载四个文字的名字
+        l1 = list(name2)
+        img = Image.open(src)
+        # 按照四张图片的大小裁剪
+        box1 = (5, 0, 17, 27)
+        box2 = (17, 0, 29, 27)
+        box3 = (29, 0, 41, 27)
+        box4 = (41, 0, 53, 27)
+        # 为每一张图片提供自己的文件夹
+        path1 = dstpath + '/%s' % l1[0]
+        path2 = dstpath + '/%s' % l1[1]
+        path3 = dstpath + '/%s' % l1[2]
+        path4 = dstpath + '/%s' % l1[3]
+        # 创建对应的文件夹
+        if not os.path.exists(path1):
+            os.makedirs(path1)
+        if not os.path.exists(path2):
+            os.makedirs(path2)
+        if not os.path.exists(path3):
+            os.makedirs(path3)
+        if not os.path.exists(path4):
+            os.makedirs(path4)
+        # 裁剪图片并保存
+        img.crop(box1).resize((28, 28), Image.ANTIALIAS).save(path1 + '/%s.png' % uuid.uuid1())
+        img.crop(box2).resize((28, 28), Image.ANTIALIAS).save(path2 + '/%s.png' % uuid.uuid1())
+        img.crop(box3).resize((28, 28), Image.ANTIALIAS).save(path3 + '/%s.png' % uuid.uuid1())
+        img.crop(box4).resize((28, 28), Image.ANTIALIAS).save(path4 + '/%s.png' % uuid.uuid1())
+
+
+if __name__ == '__main__':
+    # 原图片路径
+    root_path = '../images/src_yanzhengma/'
+    # 裁剪后图片的路径
+    dstpath = '../images/dst_yanzhengma/'
+    # 获取所以图片
+    imgs = os.listdir(root_path)
+    yanZhenMaUtil = YanZhenMaUtil()
+    # 开始裁剪
+    for src in imgs:
+        src = root_path + src
+        yanZhenMaUtil.splitimage(src=src, dstpath=dstpath)
---
+++ note5/code/DownloadYanZhengMa.py
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*-
+import re
+import uuid
+import requests
+import os
+
+
+class DownloadYanZhengMa:
+    def __init__(self,save_path,download_max,url):
+        self.download_sum = 0
+        self.save_path = save_path
+        self.download_max = download_max
+        self.url = url
+
+    def downloadImages(self):
+        try:
+            pic = requests.get(self.url, timeout=500)
+            pic_name = self.save_path+'/' +str(uuid.uuid1()) + '.png'
+            with open(pic_name, 'wb') as f:
+                f.write(pic.content)
+            self.download_sum += 1
+            print '已下载完成'+str(self.download_sum)+'张验证码'
+            if self.download_sum >= self.download_max:
+                return
+            else:
+                return self.downloadImages()
+        except  Exception, e:
+            print '【错误】当前图片无法下载，%s' % e
+            return self.downloadImages()
+
+
+if __name__ == '__main__':
+    # 下载验证码的保存路径
+    save_path = '../images/download_yanzhengma'
+    # 下载验证码的数量
+    download_max = 1000
+    # 下载验证码的网址
+    url = 'http://jw.xhsysu.cn/CheckCode.aspx?'
+    downloadYanZhenMa = DownloadYanZhengMa(save_path=save_path, download_max=download_max,url=url)
+    downloadYanZhenMa.downloadImages()
